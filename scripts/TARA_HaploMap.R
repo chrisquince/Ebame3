15aa261e5584d0a9ad3b78ac52d2931699608fc6
Christopher Quince <u1375038@004851.local>
2017-10-23 23:13:43 +0200

    Update

--- scripts/TARA_HaploMap.R
+++ scripts/TARA_HaploMap.R
@@ -4,38 +4,41 @@ library(reshape2)
 library(maps)
 library(plyr)
 library(gridExtra)
+library(getopt)
 
-##########################################################
-# Set this directory to where your TARA-*.txt files are :)
-##########################################################
-setwd("/Users/u1375038/repos/Ebame3/2017_DESMAN")
+spec = matrix(c('verbose','v',0,"logical",'help','h',0,"logical",'gammafile','g',1,"character",'samplefile','s',1,"character",'clusterfile','c',1,"character"),byrow=TRUE,ncol=4)
+
+opt=getopt(spec)
+
+# if help was asked for print a friendly message 
+# and exit with a non-zero error code 
+if( !is.null(opt$help)) {
+  cat(getopt(spec, usage=TRUE)); 
+  q(status=1);
+}
+
 
 gen_blank_world_map <- function(df) {
   world_map <- map_data("world")
   
   min_lat <- -60
-  max_lat <- 70
+  max_lat <- 45
   min_lon <- -155
   max_lon <- 75
   
   p <- ggplot(data=world_map,aes(x=long, y=lat, group=group))
   p <- p + geom_polygon(fill = '#777777') + coord_cartesian(xlim = c(min_lon, max_lon), ylim = c(min_lat, max_lat))  
-  p <- p + scale_colour_manual(values = c("Gr01" = "brown1", "Gr02" = "black", "Gr01_A" = "darkorchid", "Gr01_B" = "TAN", "Gr01_C" = "seagreen3", "Gr02_A" = "brown1", "Gr02_B" = "gold2", "Gr02_C" = "cadetblue3", "Gr09" = "plum", "Gr10" = "moccasin", "Other" = "gray88"))
   return(p)
 }
 
-add_TARA_sampling_locations <- function(plot_object, tara_samples, size = 5, alpha=0.2, labels = TRUE){
+add_TARA_sampling_locations <- function(plot_object, tara_samples, selectHaplo, size = 5, alpha=0.2, labels = TRUE){
+  print(selectHaplo)
   plot_object <- plot_object + geom_jitter(data = tara_samples,
                                            position=position_jitter(width=0, height=0),
-                                           aes(x=Mean_Long, y=Mean_Lat, group=cluster_id, size=TARA_MED_MAG_00110, color=cluster_id), alpha=alpha, fill="gray")
-  plot_object <- plot_object + scale_size(range=c(2, 15))
+                                           aes_string(x="Mean_Long", y="Mean_Lat", group="cluster_id", size=selectHaplo,alpha=selectHaplo), colour="red")
+                                           #aes(x=Mean_Long, y=Mean_Lat, group=cluster_id, size=selectHaplo, alpha=selectHaplo), colour="red")
+  plot_object <- plot_object + scale_size(range=c(0.5, 16), limits=c(0.0, 1), breaks=c(0, 0.25, 0.5, 0.75, 1))
 
-  if (labels){
-    plot_object <- plot_object + geom_text(data = tara_samples,
-                                           aes(x=Mean_Long, y=Mean_Lat, group=ocean, label=label),
-                                           size=0, hjust = 0, nudge_x = 1, fontface = "bold")
-  }
-  
   return(plot_object)
 }
 
@@ -50,12 +53,29 @@ clean_map <- function(plot_object){
 }
 
 
-# GET TARA samples data frame
-tara_samples <- read.table(file = 'TARA-samples.txt', header = TRUE , sep = "\t" , quote = "")
-#tara_samples[tara_samples$Mean_Depth > 0, ]$Mean_Lat <- tara_samples[tara_samples$Mean_Depth > 10, ]$Mean_Lat - 0
 
 # GET TARA CLUSTERS
-tara_clusters <- read.table(file = 'TARA-clusters.txt', header = TRUE , sep = "\t" , quote = "")
+tara_clusters <- read.table(file = opt$clusterfile, header = TRUE , sep = "\t" , quote = "")
+tara_samples <- read.table(file = opt$samplefile, header = TRUE , sep = "\t" , quote = "")
+
+gamma <- read.csv(opt$gammafile,header=TRUE,row.names=1)
+
+colnames(gamma) <- gsub("X","H",colnames(gamma))
+rownames(gamma) <- gsub("m","M",rownames(gamma))
+rownames(gamma) <- gsub("_5M","_05M",rownames(gamma))
+rownames(tara_samples) <- tara_samples$samples 
+gamma$samples <- rownames(gamma)
+
+tara_samples <- merge(tara_samples, gamma, by="samples",all.x = TRUE)
+
+rownames(tara_samples) <- tara_samples$samples
+gamma$samples <- NULL
+
+G <- ncol(gamma)
+T <- ncol(tara_samples)
+S <- T-G
+tara_samples[, S:T][is.na(tara_samples[, S:T])] <- 0
+
 
 # ADD LABELS and CLUSTER IDs TO TARA SAMPLES DATA FRAME
 tara_samples$label <- "UNKNOWN LABEL"
@@ -74,12 +94,19 @@ for (sample in levels(tara_samples$samples)){
 tara_samples <- tara_samples[tara_samples$Mean_Depth < 17, ]
 tara_samples$samples <- factor(tara_samples$samples)
 
-# GENERATE THE PLOT OBJECT
-world_map <- gen_blank_world_map(tara_samples)
-world_map <- add_TARA_sampling_locations(world_map, tara_samples, alpha=1)
-world_map <- clean_map(world_map)
+for (H in 0:G-1){
+  hid <- sprintf("H%d", H)
+  filename <- sprintf("WorldMap_H%d.pdf", H)
+  print(filename)
 
-# SAVE IT
-pdf('tara_world.pdf', width=16, height=16)
-print(world_map)
-dev.off()
+  # GENERATE THE PLOT OBJECT
+  world_map <- gen_blank_world_map(tara_samples)
+  world_map <- add_TARA_sampling_locations(world_map, tara_samples, selectHaplo=hid,alpha=1)
+  world_map <- clean_map(world_map)
+  
+  # SAVE IT
+  pdf(filename, width=12, height=5)
+  print(world_map + guides(alpha=FALSE))
+  dev.off()  
+}
+
